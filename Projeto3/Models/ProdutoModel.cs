961e39cba4788b3aa30e0a33338886f540e9df0c
LUIZ OLIVEIRA <luiz.oliveira159@fatec.sp.gov.br>
2022-02-25 19:48:10 -0300

    Projeto Sexta feira PW

---
+++ Projeto3/Models/ProdutoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto3.Models
+{
+    public class ProdutoModel
+    {
+        public decimal Valor { get; set; }
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
